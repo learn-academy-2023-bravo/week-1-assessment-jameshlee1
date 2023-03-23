83b9e62ee1b84b0571ceb59056453f04cd2757b9
jameshlee1 <james.h.lee3@outlook.com>
2023-03-23 09:37:32 -0700

    All Assessment Completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -17,16 +17,17 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: the variable cohort equals "LEARN 2023" for the console.log cohort.length it is giving the total characters length of the string which in this case is 10 including the space.
+
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"o"
+// b) Verify and explain: the variable greeting = "Hello world!" for the console.log(greeting[4]) it is asking for the fourth index letter of the element in the array. Starting from "0" which in this case the fourth letter is o in hello.
 
 // --------------------3) What will this log?
 
@@ -34,21 +35,23 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: const languages = "Javascript, Ruby, Python, C++" and const index = 1. for the console.log(languages[index]) because the variable index equals one it is asking for the first index of the variable languages. starting from 0 the first word would be Ruby. 
+
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: const weekendDays = "saturday, sunday" for the console.log(weekendDays/toUpperCase()) the method toUppercase returns the calling string value to uppercase not the array.
+
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 3
+// b) Verify and explain: const dataTypes = "number, string, boolean, undefined" for the console.log(typeof dataTypes.length); it is identifying the type of data from the original variable data type then it it asking for the length of the string.
